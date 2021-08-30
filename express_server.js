0d4f8ced61d8193eb5a248cbb9e710d86d3a261b
Paola Garcia <pacri14@gmail.com>
2021-08-30 20:22:39 +0000

    URL table template added

--- express_server.js
+++ express_server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
+// URL databases to keep track of the URLs and their shortened form
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/home", (req, res) => {
+  res.send("Welcome home")
 });
 
 app.get("/urls.json", (req, res) => {
@@ -17,6 +20,15 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+/**
+ * Actual routes for the project
+ */
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.listen(PORT, () => {
