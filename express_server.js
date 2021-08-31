baca0e59a8f077d343a9539cb71e1ec70ab31fd2
Paola Garcia <pacri14@gmail.com>
2021-08-31 18:32:13 +0000

    Add delete URL endpoint and button

--- express_server.js
+++ express_server.js
@@ -7,13 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-
-// URL databases to keep track of the URLs and their shortened form
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
+ 
 const generateRandomString = () => {
   const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   let randomString = '';
@@ -21,24 +15,35 @@ const generateRandomString = () => {
   for (let i = 0; i < length; i++) {
     randomString += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-
   return randomString;
 }
 
+// URL databases to keep track of the URLs and their shortened form
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+//       ROUTES
+
+//Lists all the URLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+//Renders page to create new URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Search for url by ShortURL (key)
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+//Create new URL record and saves it in the DB
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
@@ -47,11 +52,17 @@ app.post("/urls", (req, res) => {
 
 });
 
-/**
- * Redirect URLs
- */
+//Delete URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;  
+  delete urlDatabase[shortURL]; 
+  res.redirect(`/urls/`);
 
+});
+
+/// Redirect URLs 
 app.get("/u/:shortURL", (req, res) => {
+
   const longURL = urlDatabase[req.params.shortURL];
   if (!longURL){
     res.status(404).send({ error: 'Short URL not found!' })
