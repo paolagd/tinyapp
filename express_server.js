6e103d90b39b7081225da97d0deefbfbb3a71327
Paola Garcia <pacri14@gmail.com>
2021-08-31 22:05:44 +0000

    Testing changes

--- express_server.js
+++ express_server.js
@@ -101,9 +101,9 @@ app.post("/login", (req, res) => {
   res.redirect(`/urls/`);
 });
 
-//LOGIN
+//LOGOUT
 app.post("/logout", (req, res) => {
-  res.clearCookie('username')
+  res.clearCookie('username') 
   res.redirect(`/urls/`);
 });
 
